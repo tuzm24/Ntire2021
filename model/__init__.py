861cff08cbf2b497aded59cc6529d1daa2ef9f96
YangwooKim <tuzm24@naver.com>
2021-02-15 18:09:07 +0900

    yuv_net_2 bug fix

--- model/__init__.py
+++ model/__init__.py
@@ -81,6 +81,11 @@ class Model(nn.Module):
                 os.path.join(apath, 'model_latest.pt'),
                 **kwargs
             )
+        if resume == -2:
+            load_from = torch.load(
+                os.path.join(apath, 'model_best.pt'),
+                **kwargs
+            )
         elif resume == 0:
             if pre_train == 'download':
                 print('Download the model')
